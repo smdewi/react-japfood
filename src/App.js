a68a18507be049e064eaea3558357405d3793972
smdewi <smdewi2020@gmail.com>
2022-12-05 14:00:03 +0800

    Move functional components into separate files

--- src/App.js
+++ src/App.js
@@ -2,40 +2,9 @@
 import React from 'react';
 import './App.css';
 
-function Nav() {
-  return (
-    <nav>
-        <ul id="menu">
-          <li><a href="#">Home</a></li>
-          <li><a href="#about-us">About Us</a></li>
-          <li><a href="#">Our Menu</a></li>
-          <li><a href="#">Contact Us</a></li>
-        </ul>
-      </nav>
-  )
-}
-
-function Hero() {
-  return (
-    <section id="hero">
-        <div id="cta">
-          <a href="#">
-            Make a Booking
-          </a>
-        </div>
-      </section>
-  )
-}
-
-function AboutUs() {
-  return (
-    <section id="about-us">
-        <div class="content">
-          <h1>Japanese so fresh, any fresh you have to fly to Japan</h1>
-        </div>
-      </section>
-  )
-}
+import Nav from "./Nav";
+import Hero from "./Hero";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
