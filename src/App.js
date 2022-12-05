496dcfd149964eae8b8272c6968795b678f407f4
smdewi <smdewi2020@gmail.com>
2022-12-05 13:48:13 +0800

    Separate section into separate function/components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function Nav() {
   return (
-    <div className="App">
-      <nav>
+    <nav>
         <ul id="menu">
           <li><a href="#">Home</a></li>
           <li><a href="#about-us">About Us</a></li>
@@ -12,22 +12,42 @@ function App() {
           <li><a href="#">Contact Us</a></li>
         </ul>
       </nav>
+  )
+}
 
-      <section id="hero">
+function Hero() {
+  return (
+    <section id="hero">
         <div id="cta">
           <a href="#">
             Make a Booking
           </a>
         </div>
       </section>
+  )
+}
 
-      <section id="about-us">
+function AboutUs() {
+  return (
+    <section id="about-us">
         <div class="content">
           <h1>Japanese so fresh, any fresh you have to fly to Japan</h1>
         </div>
       </section>
+  )
+}
 
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-pascal-case
+    <React.Fragment>
+      <div className="App">
+        <Nav />
+        <Hero />
+        <AboutUs />
     </div>
+    </React.Fragment>
+    
   );
 }
 
