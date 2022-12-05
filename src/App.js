f8cbfd72bb5bed576bcbdb52b8299659138c7e65
smdewi <smdewi2020@gmail.com>
2022-12-05 11:54:06 +0800

    Convert replit japfood into React app, base code

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul id="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#about-us">About Us</a></li>
+          <li><a href="#">Our Menu</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+
+      <section id="hero">
+        <div id="cta">
+          <a href="#">
+            Make a Booking
+          </a>
+        </div>
+      </section>
+
+      <section id="about-us">
+        <div class="content">
+          <h1>Japanese so fresh, any fresh you have to fly to Japan</h1>
+        </div>
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
